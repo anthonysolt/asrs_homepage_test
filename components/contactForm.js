e4226dde198bd773413a7a0020c56339685ceea0
senshi jif <alsolt@syr.edu>
2026-06-30 18:16:45 -0400

    class DOM error fix

--- components/contactForm.js
+++ components/contactForm.js
@@ -1,6 +1,6 @@
-export default function VolunteerForm() {
+export default function ContactForm() {
   return (
-    <form>
+    <form action="/contact">
       <div className="space-y-12">
         <div className="border-b border-white/10 pb-12">
           <h2 className="text-base/7 font-semibold text-white"> Contact Form </h2>
@@ -8,55 +8,38 @@ export default function VolunteerForm() {
 
           <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
-              <label htmlFor="first-name" className= "block text-sm/6 font-semibold text-white">
-                First name
-              </label>
+              <label htmlFor="first_name" className= "block text-sm/6 font-semibold text-white"> First name </label>
               <div className="mt-2">
                 <input
-                  id="first-name"
-                  name="first-name"
-                  type="text"
-                  autoComplete="given-name"
+                  type="text" name="first_name" id="first_name" placeholder="John" autoComplete="first_name " required
                   className="block w-full rounded-md bg-asrs-lightInk px-3 py-1.5 text-white outline-none hover:outline hover:outline-2 hover:outline-white focus:outline-solid  focus:outline-2 focus:outline-white"/>
               </div>
             </div>
 
 
             <div className="sm:col-span-3">
-              <label htmlFor="last-name" className="block text-sm/6 font-semibold text-white">
-                Last name
-              </label>
+              <label htmlFor="last_name" className="block text-sm/6 font-semibold text-white"> Last name </label>
               <div className="mt-2">
                 <input
-                  id="last-name"
-                  name="last-name"
-                  type="text"
-                  autoComplete="family-name"
+                  type="text" name="last_name" id="last_name" placeholder="Doe" autoComplete="last_name" required
                   className="block w-full rounded-md bg-asrs-lightInk px-3 py-1.5 text-white outline-none hover:outline hover:outline-2 hover:outline-white focus:outline-solid  focus:outline-2 focus:outline-white"/>
-
               </div>
             </div>
 
             <div className="sm:col-span-3 sm:col-start-1">
-              <label htmlFor="email" className="block text-sm/6 font-semibold text-white"> Email address </label>
+              <label htmlFor="email" className="block text-sm/6 font-semibold text-white"> Email address  </label>
               <div className="mt-2">
                 <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
+                  type="email" name="email" id="email" placeholder="john.doe@example.com" autoComplete="email" required
                   className="block w-full rounded-md bg-asrs-lightInk px-3 py-1.5 text-white outline-none hover:outline hover:outline-2 hover:outline-white focus:outline-solid  focus:outline-2 focus:outline-white"/>
               </div>
             </div>
 
             <div className="sm:col-span-3 ">
-              <label htmlFor="phoneNum" className="block text-sm/6 font-semibold text-white"> Phone Number </label>
+              <label htmlFor="phone_number" className="block text-sm/6 font-semibold text-white"> Phone Number </label>
               <div className="mt-2">
                 <input
-                  id="phoneNum"
-                  name="phoneNum"
-                  type="tel"
-                  autoComplete="tel"
+                  type="tel" name="phone_number" id="phone_number" placeholder="(123) 456-7890" autoComplete="tel" required
                   className="block w-full rounded-md bg-asrs-lightInk px-3 py-1.5 text-white outline-none hover:outline hover:outline-2 hover:outline-white focus:outline-solid  focus:outline-2 focus:outline-white"/>
               </div>
             </div>
@@ -65,15 +48,13 @@ export default function VolunteerForm() {
       </div>
 
       <div className="mt-6 flex items-center justify-end gap-x-6">
-        <button type="button" className="text-sm/6 font-semibold text-white">
-          Cancel
-        </button>
-        <button
-          type="submit"
-          className="rounded-md bg-asrs-orange px-3 py-2 text-sm/6 font-semibold text-white shadow-sm hover:bg-asrs-orangeDark focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-asrs-orange"
-        >
+        <button type="reset" className="text-sm/6 font-semibold text-white"> 
+          Cancel 
+          </button>
+        
+        <button type="submit" className="rounded-md bg-asrs-orange px-3 py-2 text-sm/6 font-semibold text-white shadow-sm hover:bg-asrs-orangeDark focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-asrs-orange">
           Submit
-        </button>
+          </button>
       </div>
     </form>
   )
